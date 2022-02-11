a52cdb3f0c57d29eddfd7e2831e7d2a670b12022
Zeri <41052126+jyejyes@users.noreply.github.com>
2022-02-11 20:51:50 +0900

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,18 +1,66 @@
-const inputId = document.getElementById("id");
-const moreId = document.getElementById("click_more_id");
-const inputPw = document.getElementById("password");
-const morePw = document.getElementById("click_more_pw");
-const inputPwCheck = document.getElementById("password_check");
-const morePwCheck = document.getElementById("click_more_pwc");
+"use strict";
 
-inputId.addEventListener("click", () => {
+const id = document.getElementById("id");
+const password = document.getElementById("password");
+const passwordCheck = document.getElementById("passwordCheck");
+const moreId = document.getElementById("more_id");
+const morePw = document.getElementById("more_pw");
+const morePwc = document.getElementById("more_pwc");
+
+const addrBtn = document.querySelector(".addBtn");
+const addressBox = document.querySelector(".address_box");
+const addressInput = document.querySelectorAll(".address");
+const addrDetail = document.getElementById("address_2");
+const reSearch = document.getElementById("researchBtn");
+const recommendId = document.getElementById("recommend_id");
+const eventName = document.getElementById("event");
+const addEventBox = document.querySelector(".add_event_box");
+const eventInput = document.querySelector(".event_input");
+
+id.addEventListener("click", () => {
   moreId.style.display = "flex";
 });
 
-inputPw.addEventListener("click", () => {
+password.addEventListener("click", () => {
   morePw.style.display = "flex";
 });
 
-inputPwCheck.addEventListener("click", () => {
-  morePwCheck.style.display = "flex";
+passwordCheck.addEventListener("click", () => {
+  morePwc.style.display = "flex";
+});
+
+recommendId.addEventListener("click", () => {
+  addEventBox.style.display = "inline";
+  eventInput.placeholder = "추천인 아이디를 입력해주세요.";
+});
+
+eventName.addEventListener("click", () => {
+  addEventBox.style.display = "inline";
+  eventInput.placeholder = "참여 이벤트명을 입력해주세요.";
+});
+
+//주소찾기
+
+console.log(addressInput);
+
+addrBtn.addEventListener("click", () => {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      addressInput[0].value = data.address + " (" + data.buildingName + ")";
+      addressBox.style.display = "inline";
+      addrBtn.style.display = "none";
+      addrDetail.focus();
+    },
+  }).open();
+});
+
+reSearch.addEventListener("click", () => {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      addressInput[0].value = data.address + " (" + data.buildingName + ")";
+      addressBox.style.display = "inline";
+      addrBtn.style.display = "none";
+      addrDetail.focus();
+    },
+  }).open();
 });
